18ee15fc4c13768b52d1163de4021e565bae9933
iKnock <ergana.henok@gmail.com>
2021-06-20 15:57:17 +0200

    search by skill

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -41,6 +41,19 @@ module.exports = app => {
 
     });
 
+    app.get('/api/skillChasers/users/skills/:skill', verifyAccessToken, async (req, res) => {
+        Logger.info('<User Route: > - Read user by skills : ' + req.ip);
+        const user = await User.findOne({
+            "skills": req.params.skill
+        });
+        if (user) {
+            res.status(200).json({ "status": "OK", "error": "{}", "payload": user });
+        } else {
+            res.status(404).json({ "status": "KO", "error": "Skill not found", "payload": "{}" });
+        }
+
+    });
+
     app.get('/api/skillChasers/users/name/:userName', verifyAccessToken, async (req, res) => {
         Logger.info('<User Route: > - Read user by user name : ' + req.ip);
         const user = await getUserByName(req.ip);
