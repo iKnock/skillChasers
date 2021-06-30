22c012a9e5dded197a55b21f775b6c0c3b2d25f4
iKnock <ergana.henok@gmail.com>
2021-06-30 20:18:03 +0200

    payload too large fix

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.use(
 
 app.use('*', cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser({ limit: '5mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(passport.initialize());
 app.use(passport.session());
