6ecedd65fbde6ef25bc9c91ea1cc97684984a4a2
iKnock <ergana.henok@gmail.com>
2024-09-19 15:07:30 +0200

    update docker-compose and mongo uri

--- config/dev.mjs
+++ config/dev.mjs
@@ -3,6 +3,7 @@ dotenv.config();
 
 export const logDirName = "skillchaserslog";
 export const mongoURI = "mongodb://mongodb:27017/skillchasersdb";
+//export const mongoURI = "mongodb://mongodb.default.svc.cluster.local:27017/skillchasersdb";
 export const cookieKey = "cookieKey";
 export const passportSecret = "passDevSec";
 export const passportExpiresIn = 1000000;
